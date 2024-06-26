f06fe924b256e515b5aed521c89b18994b0c6cfd
fetwi <fetwi.fitwi@gmail.com>
2024-06-26 09:46:29 -0400

    modified:   Master-PN-DB.xlsm 	new file:   notifications_change.py

---
+++ notifications_change.py
@@ -0,0 +1,31 @@
+import os
+from bs4 import BeautifulSoup
+
+# Define the directory
+dir_path = os.path.dirname(os.path.realpath(__file__))
+directory = os.path.join(dir_path, "notifications")
+
+# Iterate over all files in the directory
+for filename in os.listdir(directory):
+    # Check if the file is an .html file
+    if filename.endswith('.txt'):
+        filepath = os.path.join(directory, filename)
+        with open(filepath, 'r', encoding='utf-8') as file:
+            content = file.read()
+
+        # Parse the HTML content
+        soup = BeautifulSoup(content, 'html.parser')
+
+        # Find all button tags with class "print-clause"
+        for button in soup.find_all('button', class_='print-clause'):
+            # Create a new div tag
+            button.insert_after('<div>')
+            button.insert_after('\n')
+            new_div.insert_after('\n')
+
+        # Add a closing div tag at the end of the document
+        soup.append('</div>')
+
+        # Write the modified HTML back to the file
+        with open(filepath, 'w', encoding='utf-8') as file:
+            file.write(str(soup))
